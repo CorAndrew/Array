7c49492d1cec87c1fe8a1ae74a729f519fb0eb79
AndrewCor <14529982@itisrossi.vi.it>
2026-02-11 18:21:07 +0100

    Aggiunto l'esercizio 2

---
+++ Es.2/Es.2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace es2;
+
+using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeri = new int[30];
+        Random generatore = new Random();
+
+        WriteLine("Sequenza generata:");
+        for (int i = 0; i < numeri.Length; i++)
+        {
+            numeri[i] = generatore.Next(0, 101); // Numeri tra 0 e 100
+            Write(numeri[i] + " ");
+        }
+
+        WriteLine("Coppie trovate:");
+
+        // Doppio ciclo for per confrontare ogni numero con tutti gli altri
+        for (int i = 0; i < numeri.Length; i++)
+        {
+            for (int j = 0; j < numeri.Length; j++)
+            {
+                // Controllo numeri doppi e pari
+                if (numeri[i] % 2 == 0 && numeri[i] == numeri[j] * 2)
+                {
+                    WriteLine($"{numeri[i]} / {numeri[j]}");
+                }
+            }
+        }
+    }
+}
